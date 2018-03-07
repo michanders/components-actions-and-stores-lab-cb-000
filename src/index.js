b6ab130794725ea48957f8b5dad1c7a04d9630d1
Michael Anderson <michanders@msn.com>
2018-03-07 19:39:10 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from './store';
+const store = createStore(reducer)
 
 function render(){
   ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
