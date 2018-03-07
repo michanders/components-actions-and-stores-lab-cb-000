26314e94b0251e870e3c9998d421021b68c4261e
Michael Anderson <michanders@msn.com>
2018-03-07 19:38:31 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from './store';
 
 function render(){
   ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
