911b6f30a1b5f9dd2b24d61052db8bd6325e24b5
Michael Anderson <michanders@msn.com>
2018-03-07 19:42:28 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from './store';
+import BandInput from './components/band_input_component';
 import bandReducer from './reducers/band_reducer'
 const store = createStore(reducer)
 
